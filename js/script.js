a2cbf087c6c481b93967a7414e36365689cf9db5
niccoloruberti <nikiruberti@gmail.com>
2023-05-31 16:43:43 +0200

    milestone 0

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+//creo l'array contenente tutti i membri del team con le relative informazioni
+const teamMembers = [
+    {'name': 'Wayne Barnett',
+    'role': 'Founder & CEO',
+    'image': 'wayne-barnett-founder-ceo.jpg'},
+
+    {'name': 'Angela Caroll',
+    'role': 'Chief Editor',
+    'image': 'angela-caroll-chief-editor.jpg'},
+
+    {'name': 'Walter Gordon',
+    'role': 'Office Manager',
+    'image': 'walter-gordon-office-manager.jpg'},
+
+    {'name': 'Angela Lopez',
+    'role': 'Social Media Manager',
+    'image': 'angela-lopez-social-media-manager.jpg'},
+
+    {'name': 'Scott Estrada',
+    'role': 'Developer',
+    'image': 'scott-estrada-developer.jpg'},
+
+    {'name': 'Wayne Barnett',
+    'role': 'Founder & CEO',
+    'image': 'wayne-barnett-founder-ceo.jpg'},
+
+    {'name': 'Barbara Ramos',
+    'role': 'Graphic Designer',
+    'image': 'barbara-ramos-graphic-designer.jpg'}
+]
+
+console.log(teamMembers)
